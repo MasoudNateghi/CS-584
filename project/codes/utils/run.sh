27035aeaa1801d4025cea95ecbc28516cf0e6b62
MasoudNateghi <masoudnateghi78@gmail.com>
2025-04-21 16:27:51 -0400

    Added run.sh

---
+++ project/codes/utils/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=CellTrack
+#SBATCH --output=log_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+#SBATCH --time=3-00:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+source ../../../python_venv/CS584/bin/activate
+echo "Running data preprocessing script..."
+python 01_organize_data.py
+echo "Running training script..."
+python 02_train.py
